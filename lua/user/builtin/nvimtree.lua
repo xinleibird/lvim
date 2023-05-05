e514bbadb42899a23643bf5314baf08b4733b81e
辛磊 <xinleibird@gmail.com>
2023-05-05 21:35:29 +0800

    Update nvimtree and telescope ignore filters

--- lua/user/builtin/nvimtree.lua
+++ lua/user/builtin/nvimtree.lua
@@ -1,7 +1,6 @@
 -- NvimTree settings
 lvim.builtin.nvimtree.setup.view.side               = "left"
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
-lvim.builtin.nvimtree.setup.filters.custom          = { ".DS_Store", ".AppleDouble", ".LSOverride", "Icon[^M]" }
 lvim.builtin.nvimtree.setup.git.ignore              = true
 lvim.builtin.nvimtree.setup.sync_root_with_cwd      = false
 lvim.builtin.nvimtree.setup.respect_buf_cwd         = false
@@ -9,3 +8,7 @@ lvim.builtin.nvimtree.setup.update_focused_file     = {
   enable = false,
   update_root = false
 }
+lvim.builtin.nvimtree.setup.filters.custom          = { ".DS_Store", "Icon[^M]", ".AppleDouble", ".CFUserTextEncoding",
+  "._*", ".DocumentRevisions-V100", ".fseventsd", ".Spotlight-V100", ".TemporaryItems", ".Trashes", ".VolumeIcon.icns",
+  ".com.apple.timemachine.donotpresent", ".AppleDB", ".AppleDesktop", "Network Trash Folder", "Temporary Items",
+  ".apdisk", }
--- lua/user/builtin/telescope.lua
+++ lua/user/builtin/telescope.lua
@@ -1,2 +1,7 @@
 -- Add Telescope ignore patterns
-lvim.builtin.telescope.defaults.file_ignore_patterns = { ".git/", "lock.json" }
+-- reference: <https://www.lua.org/manual/5.1/manual.html#5.4.1>
+lvim.builtin.telescope.defaults.file_ignore_patterns = { "^%.DS_Store", "^Icon$", "%.git/", "lock%.json",
+  "^%.zsh_sessions/", "^%.cache/", "^%.vscode/", "^%.vscode-cli/", "^%.zcompdump", "^%.npm/", "^%.nvm/",
+  "^%.oh%-my%-zsh/", "^Applications/", "^Movies/", "^Music/", "^%Pictures/", "^Library/", "^Public/", "^Documents/",
+  "^Downloads/", "%.DS_Store", "^%.ssh/", "^%.cargo/", "^%.rustup", "^%.lemminx/", "^%.redhat", "^%.localized",
+  "%.localized/", "^%.viminfo", "^%.bash_history", "^%.python_history" }
