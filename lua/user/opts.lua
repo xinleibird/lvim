253e53c1439e8cd85bf357ed19b8798a8b15693b
辛磊 <xinleibird@gmail.com>
2023-05-07 16:45:15 +0800

    Change guicursor style

--- lua/user/opts.lua
+++ lua/user/opts.lua
@@ -5,6 +5,7 @@ vim.opt.tabstop        = 2
 vim.opt.relativenumber = false
 vim.opt.langmenu       = "en_US.UTF-8"
 vim.opt.clipboard      = ""
+vim.opt.guicursor      = "n-v-sm:block,c-i-ci-ve:ver25,r-cr-o:hor25"
 
 
 -- Remove paste pattern for comment
