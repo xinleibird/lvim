8cf4526fae8db5b86c93bd96821fd5078e7ba703
辛磊 <xinleibird@gmail.com>
2023-05-13 03:15:24 +0800

    Update hotkey

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -1,16 +1,20 @@
--- Remap gh gH to disable <Select Mode>
+-- remap gh gH to disable <Select Mode>
 -- lvim.lsp.buffer_mappings.normal_mode['gh']   = { vim.lsp.buf.hover, "Show hover" }
 -- lvim.lsp.buffer_mappings.normal_mode['gH']   = { vim.lsp.buf.hover, "Show hover" }
 
--- Add new Map to <f2>
+-- add new Map to <f2>
 lvim.lsp.buffer_mappings.normal_mode['<f2>'] = { vim.lsp.buf.rename, "Rename all references" }
 
--- Find files like ctrl+p
+-- find files like ctrl+p
 lvim.keys.normal_mode["<D-p>"]               = ":Telescope find_files<CR>"
 lvim.keys.normal_mode["<D-o>"]               = ":SymbolsOutline<CR>"
 
+-- paste hotkey
 lvim.keys.insert_mode["<D-v>"]               = '<C-o>"+p'
-vim.keymap.set('x', '<D-v>', '<C-r>+', { buffer = true, silent = true })
 
--- Toggle terminal mapping
-lvim.builtin.terminal.open_mapping = "<c-`>"
+-- toggle terminal mapping
+lvim.builtin.terminal.open_mapping           = "<c-`>"
+vim.api.nvim_set_keymap('c', '<D-v>', '<C-r>+', { noremap = true })
+
+-- command mode move cursor to line head
+vim.api.nvim_set_keymap('c', '<C-a>', '<C-b>', { noremap = true })
