e8410d0c08aa24c02f908034866bba9510c7887d
辛磊 <xinleibird@gmail.com>
2023-08-29 13:35:28 +0800

    Update banner

--- lua/config/lvim/alpha.lua
+++ lua/config/lvim/alpha.lua
@@ -2,10 +2,10 @@
 -- lvim.builtin.alpha.active = false
 
 local greeting = require("lib.greeting").generate()
-local version = "            󰟬  󱏿          󰎙     󰜫  󰘐"
+local version = "            󰟬  󱏿          󰎙      󰜫  󰘐"
 
 lvim.builtin.alpha.dashboard.section.header.val = require("lib.banner").dashboard()
-lvim.builtin.alpha.dashboard.section.footer.val = require("lvim.interface.text").align_left(
+lvim.builtin.alpha.dashboard.section.footer.val = require("lvim.interface.text").align_center(
   { width = 0 },
   { "", "", "", greeting, version },
   0
