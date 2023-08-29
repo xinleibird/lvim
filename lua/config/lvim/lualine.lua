c97ac89ffd60cd922997847b7c1fd07f5d647b56
辛磊 <xinleibird@gmail.com>
2023-08-29 13:59:57 +0800

    Update lualine

--- lua/config/lvim/lualine.lua
+++ lua/config/lvim/lualine.lua
@@ -5,12 +5,14 @@ lvim.builtin.lualine.options.component_separators = { left = "", right = "
 local function selectionCount()
   local isVisualMode = vim.fn.mode():find "[Vv]"
   if not isVisualMode then
-    return "%P  %L"
+    return "%P  %04L"
   end
   local starts = vim.fn.line "v"
   local ends = vim.fn.line "."
   local lines = starts <= ends and ends - starts + 1 or starts - ends + 1
-  return "󰫙  " .. "󰉸 " .. tostring(lines) .. "  󰾹 " .. tostring(vim.fn.wordcount().visual_chars)
+  return string.format("%03d", tostring(lines))
+    .. " 󰆊 "
+    .. string.format("%04d", tostring(vim.fn.wordcount().visual_chars))
 end
 
 lvim.builtin.lualine.sections.lualine_z = {
