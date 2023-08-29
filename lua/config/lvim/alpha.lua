60e46715a5f419e4701a6a6d3f443fbb3875c2a8
辛磊 <xinleibird@gmail.com>
2023-08-29 13:22:57 +0800

    Update banner

--- lua/config/lvim/alpha.lua
+++ lua/config/lvim/alpha.lua
@@ -1,13 +1,12 @@
 -- Enable dashboard
 -- lvim.builtin.alpha.active = false
 
-local version = "         "
+local greeting = require("lib.greeting").generate()
+local version = "            󰟬  󱏿          󰎙     󰜫  󰘐"
 
-local greeting = require("user.lib.greeting").generate()
-
-lvim.builtin.alpha.dashboard.section.header.val = require("user.lib.banner").dashboard()
-lvim.builtin.alpha.dashboard.section.footer.val = require("lvim.interface.text").align_center(
+lvim.builtin.alpha.dashboard.section.header.val = require("lib.banner").dashboard()
+lvim.builtin.alpha.dashboard.section.footer.val = require("lvim.interface.text").align_left(
   { width = 0 },
   { "", "", "", greeting, version },
-  0.75
+  0
 )
