7e656b8f31e44d5bf01a86485112e56cfa1061e9
辛磊 <xinleibird@gmail.com>
2023-08-30 21:24:40 +0800

    Rename utils dir

--- lua/local/lvim/alpha.lua
+++ lua/local/lvim/alpha.lua
@@ -1,10 +1,10 @@
 -- Enable dashboard
 -- lvim.builtin.alpha.active = false
 
-local greeting = require("local.lib.greeting").generate()
+local greeting = require("local.utils.greeting").generate()
 local version = "            󰟬  󱏿          󰎙      󰜫  󰘐"
 
-lvim.builtin.alpha.dashboard.section.header.val = require("local.lib.banner").dashboard()
+lvim.builtin.alpha.dashboard.section.header.val = require("local.utils.banner").dashboard()
 lvim.builtin.alpha.dashboard.section.footer.val = require("lvim.interface.text").align_center(
   { width = 0 },
   { "", "", "", greeting, version },
