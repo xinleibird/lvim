549113cf7aff902033767e168b26c6bfde995543
辛磊 <xinleibird@gmail.com>
2023-09-01 16:28:05 +0800

    Update bufferline style with catppuccin

--- lua/local/lvim/bufferline.lua
+++ lua/local/lvim/bufferline.lua
@@ -1,3 +1,5 @@
 -- Active bufferline
 lvim.builtin.bufferline.active = true
 lvim.builtin.bufferline.options.always_show_bufferline = true
+
+lvim.builtin.bufferline.highlights = require("catppuccin.groups.integrations.bufferline").get()
