15c4b36bd8b11225c8b060d2f769f1ea434a49a1
辛磊 <xinleibird@gmail.com>
2023-09-03 00:38:51 +0800

    Update style

--- lua/plugins/togglesemi/lua/togglesemi.lua
+++ lua/plugins/togglesemi/lua/togglesemi.lua
@@ -1,17 +1,16 @@
 local M = {}
 
 M.toggle = function(character)
-  local api = vim.api
   local delimiters = { ",", ";" }
-  local line = api.nvim_get_current_line()
+  local line = vim.api.nvim_get_current_line()
   local last_char = line:sub(-1)
 
   if last_char == character then
-    return api.nvim_set_current_line(line:sub(1, #line - 1))
+    return vim.api.nvim_set_current_line(line:sub(1, #line - 1))
   elseif vim.tbl_contains(delimiters, last_char) then
-    return api.nvim_set_current_line(line:sub(1, #line - 1) .. character)
+    return vim.api.nvim_set_current_line(line:sub(1, #line - 1) .. character)
   else
-    return api.nvim_set_current_line(line .. character)
+    return vim.api.nvim_set_current_line(line .. character)
   end
 end
 
