054d60facde6bcbc8a7673aaaf7836bb25030259
辛磊 <xinleibird@gmail.com>
2023-09-03 18:21:44 +0800

    Update banner

--- lua/config/lvim/luasnip.lua
+++ lua/config/lvim/luasnip.lua
@@ -9,7 +9,6 @@
 --    h:v_CTRL-G
 -- reference:
 --    <https://github.com/L3MON4D3/LuaSnip/blob/a46ab6f8bce6bbddd8ce3e287e1a517c1f5e348e/DOC.md#variables>
-
 local ok, luasnip = pcall(require, "luasnip")
 if ok then
   luasnip.config.setup { store_selection_keys = "<Tab>" }
