2cf9332a0950e2b88c57168893e5f2b823a5cde1
辛磊 <xinleibird@gmail.com>
2023-09-12 14:23:26 +0800

    Update null-ls settings

--- lua/config/lvim/null-ls.lua
+++ lua/config/lvim/null-ls.lua
@@ -1,26 +1,3 @@
-local ok, null_ls = pcall(require, "null-ls")
-if ok then
-  local helpers = require "null-ls.helpers"
-  local FORMATTING = require("null-ls.methods").internal.FORMATTING
-
-  null_ls.register {
-    --your custom sources go here
-    helpers.make_builtin {
-      name = "stopnoise",
-      meta = {
-        url = "https://github.com/jose-elias-alvarez/null-ls.nvim",
-        description = "Stop [NO formatter] notify noise.",
-      },
-      method = FORMATTING,
-      filetypes = { "vim" },
-      generator_opts = {
-        -- do nothing……
-      },
-      factory = helpers.formatter_factory,
-    },
-  }
-end
-
 lvim.lsp.null_ls.setup.ensure_installed = {
   "eslint_d",
   "goimports",
@@ -29,7 +6,6 @@ lvim.lsp.null_ls.setup.ensure_installed = {
   "shellcheck",
   "shfmt",
   "stylua",
-  "vint",
 }
 
 -- Formatters
@@ -48,7 +24,6 @@ require("lvim.lsp.null-ls.linters").setup {
   { name = "eslint_d" },
   { name = "jsonlint" },
   { name = "shellcheck", args = { "--severity", "warning" } },
-  { name = "vint" },
 }
 
 -- Actions e.g. fix issues
