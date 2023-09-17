e454cca21011b66b4af3a40467fe3ab736cd0f10
辛磊 <xinleibird@gmail.com>
2023-09-17 23:51:15 +0800

    Update fidget

--- lua/lazy/fidget.lua
+++ lua/lazy/fidget.lua
@@ -35,12 +35,6 @@ local M = {
         end,
         -- function to format each task line
         task = function(task_name, message, percentage)
-          if #message > 42 then
-            message = string.format("%.39s...", message)
-          end
-          if #task_name > 42 then
-            task_name = string.format("%.39s...", task_name)
-          end
           return string.format(
             "%s [%s] %s ",
             percentage and string.format(" (%s%%)", percentage) or "",
