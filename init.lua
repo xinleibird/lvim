8088684daff8e3a1eae7b3ad9f21abe40c7ebf01
辛磊 <xinleibird@gmail.com>
2023-09-22 09:47:45 +0800

    Enable vim loader

--- init.lua
+++ init.lua
@@ -4,6 +4,10 @@ if vim.g.vscode then
   -- Just for VSCode-Neovim
   vim.cmd.runtime "vscode.vim"
 else
+  -- Enable loader to reduce plugin load time
+  if vim.loader then
+    vim.loader.enable()
+  end
   -- Initialize LunarVim like e.g. ~/.local/share/nvim/lvim/init.lua
   local lvim_runtime = os.getenv "LUNARVIM_BASE_DIR" .. "/init.lua"
   dofile(lvim_runtime)
