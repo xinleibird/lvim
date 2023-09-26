9cc6f43ecec0682877fef8586f884bdc56deb381
辛磊 <xinleibird@gmail.com>
2023-09-27 00:56:22 +0800

    Update lualine

--- lua/config/lvim/lualine.lua
+++ lua/config/lvim/lualine.lua
@@ -3,28 +3,36 @@ lvim.builtin.lualine.options.theme = "catppuccin"
 lvim.builtin.lualine.options.section_separators = { left = "", right = "" }
 lvim.builtin.lualine.options.component_separators = { left = "", right = "" }
 
-local function selectionCount()
+local function isNormalMode()
   local mode = vim.fn.mode()
-  local isVisualMode = mode == "V" or mode == "v" or mode == "CTRL-V" or mode == "\22"
-  if isVisualMode then
+  return mode == "n" or mode == "niI" or mode == "niR" or mode == "niV" or mode == "nt" or mode == "ntT"
+end
+
+local function isVisualMode()
+  local mode = vim.fn.mode()
+  return mode == "V" or mode == "v" or mode == "CTRL-V" or mode == "\22"
+end
+
+local function selectionCount()
+  local isVisual = isVisualMode()
+  if isVisual then
     local starts = vim.fn.line "v"
     local ends = vim.fn.line "."
     local lines = starts <= ends and ends - starts + 1 or starts - ends + 1
     return string.format("%03d", tostring(lines))
-      .. " 󰯈 "
+      .. "  "
       .. string.format("%04d", tostring(vim.fn.wordcount().visual_chars))
   end
 
-  return "%P 󰚌 %04L"
+  return "%P 󰮯 %04L"
 end
 
 lvim.builtin.lualine.sections.lualine_a = {
   {
     "mode",
     fmt = function()
-      local mode = vim.fn.mode()
-      local isVisualMode = mode == "V" or mode == "v" or mode == "CTRL-V" or mode == "\22"
-      if isVisualMode then
+      local isNormal = isNormalMode()
+      if not isNormal then
         return "󱙝"
       end
       return "󰊠"
