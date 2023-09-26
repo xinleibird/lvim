ed49d4d3b520fe95858fc05de7acd5a43e234c12
辛磊 <xinleibird@gmail.com>
2023-09-26 16:53:35 +0800

    Bump plugins

--- lua/config/lvim/null-ls.lua
+++ lua/config/lvim/null-ls.lua
@@ -38,13 +38,11 @@ require("lvim.lsp.null-ls.formatters").setup {
 
 -- Linters
 require("lvim.lsp.null-ls.linters").setup {
-  -- { name = "eslint" },
   { name = "jsonlint" },
   { name = "shellcheck", args = { "--severity", "warning" } },
 }
 
 -- Actions e.g. fix issues
 require("lvim.lsp.null-ls.code_actions").setup {
-  -- { name = "eslint" },
   { name = "shellcheck" },
 }
