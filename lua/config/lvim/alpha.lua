5bb71fd2872903c581f6c879f520362421e07ec6
辛磊 <xinleibird@gmail.com>
2023-10-24 06:38:00 +0800

    Bump plugins

--- lua/config/lvim/alpha.lua
+++ lua/config/lvim/alpha.lua
@@ -5,13 +5,19 @@ local pacman =
   "󰮯············································󱙝󰊠󱙝"
 -- local greeting = require("utils.greeting").generate()
 local skills = "            󰟬  󱏿          󰎙      󰜫  󰘐"
-local version = " Neovim                                  "
+
+local lvim_version = require("lvim.utils.git").get_lvim_branch()
+
+local version = " Neovim ver "
   .. vim.version().major
   .. "."
   .. vim.version().minor
   .. "."
   .. vim.version().patch
-  .. " 󰥱"
+  .. "              "
+  .. lvim_version
+  .. " LunarVim"
+  .. " "
 
 lvim.builtin.alpha.dashboard.section.header.val = require("utils.banner").dashboard()
 lvim.builtin.alpha.dashboard.section.footer.val = require("lvim.interface.text").align_center(
--- lua/config/nvim/autocmd.lua
+++ lua/config/nvim/autocmd.lua
@@ -49,12 +49,12 @@ vim.api.nvim_create_autocmd("VimLeave", {
   command = 'set guicursor= | call chansend(v:stderr, "\x1b[ q")',
 })
 
--- Close buffer use <esc> and q
+-- Close lazy buffer use <esc> and q
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "dap-repl", "lazy", "NvimTree", "Trouble" },
+  pattern = { "lazy" },
   group = vim.api.nvim_create_augroup("user_add_quit_hotkey", { clear = true }),
   -- command = "nnoremap <buffer><silent> <Esc> <CMD>close!<CR>|nnoremap <buffer><silent> q <CMD>close!<CR>",
-  command = "nnoremap <buffer><silent> q <CMD>close!<CR>",
+  command = "nnoremap <buffer><silent> <Esc> <CMD>close!<CR>",
 })
 
 -- Fixed qf win height
