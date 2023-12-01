06529904b290bb592fbd79b4263763fe6654ea96
辛磊 <xinleibird@gmail.com>
2023-12-01 08:05:21 +0800

    Bump plugins

--- lua/lazy/gitlazy.lua
+++ lua/lazy/gitlazy.lua
@@ -2,13 +2,12 @@ local M = {
   {
     "gitlazy",
     dir = get_config_dir() .. "/plugins/gitlazy",
-    event = "VeryLazy",
     dependencies = {
       "vimpostor/vim-lumen",
     },
     config = function()
       require("gitlazy").setup()
-      -- lvim.builtin.which_key.mappings.g.g = { "<CMD>lua require 'gitlazy'.open()<CR>", "Lazygit" }
+      lvim.builtin.which_key.mappings.g.g = { "<CMD>lua require 'gitlazy'.open()<CR>", "GitLazy" }
     end,
   },
 }
--- lua/lazy/neogit.lua
+++ lua/lazy/neogit.lua
@@ -1,37 +1,38 @@
 local M = {
-  {
-    "NeogitOrg/neogit",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope.nvim",
-      "sindrets/diffview.nvim",
-    },
-    config = function()
-      require("neogit").setup {
-        signs = {
-          -- { CLOSED, OPENED }
-          hunk = { "", "" },
-          item = { "", "" },
-          section = { "", "" },
-        },
-        kind = "auto",
-      }
+  -- {
+  --   "NeogitOrg/neogit",
+  --   dependencies = {
+  --     "nvim-lua/plenary.nvim",
+  --     "nvim-telescope/telescope.nvim",
+  --     "sindrets/diffview.nvim",
+  --     "ibhagwan/fzf-lua",
+  --   },
+  --   config = function()
+  --     require("neogit").setup {
+  --       signs = {
+  --         -- { CLOSED, OPENED }
+  --         hunk = { "", "" },
+  --         item = { "", "" },
+  --         section = { "", "" },
+  --       },
+  --       kind = "auto",
+  --     }
 
-      lvim.builtin.which_key.mappings.g.g = { "<CMD>Neogit<CR>", "Neogit" }
+  --     lvim.builtin.which_key.mappings.g.g = { "<CMD>Neogit<CR>", "Neogit" }
 
-      vim.api.nvim_create_autocmd("User", {
-        pattern = {
-          "NeogitStatusRefreshed",
-          "NeogitCommitComplete",
-          "NeogitPushComplete",
-          "NeogitPullComplete",
-          "NeogitFetchComplete",
-        },
-        group = vim.api.nvim_create_augroup("user_geogit_auto_refresh_nvimtree", { clear = true }),
-        command = 'if exists(":NvimTreeRefresh") | exe "NvimTreeRefresh" | endif',
-      })
-    end,
-  },
+  --     vim.api.nvim_create_autocmd("User", {
+  --       pattern = {
+  --         "NeogitStatusRefreshed",
+  --         "NeogitCommitComplete",
+  --         "NeogitPushComplete",
+  --         "NeogitPullComplete",
+  --         "NeogitFetchComplete",
+  --       },
+  --       group = vim.api.nvim_create_augroup("user_geogit_auto_refresh_nvimtree", { clear = true }),
+  --       command = 'if exists(":NvimTreeRefresh") | exe "NvimTreeRefresh" | endif',
+  --     })
+  --   end,
+  -- },
 }
 
 return M
