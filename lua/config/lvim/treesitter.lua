e1703b016cc8077813c71a1449113114839d9e0a
辛磊 <xinleibird@gmail.com>
2024-01-24 03:58:19 +0800

    Correct spelling error

--- lua/config/lvim/treesitter.lua
+++ lua/config/lvim/treesitter.lua
@@ -5,5 +5,5 @@ lvim.builtin.treesitter.ignore_install = { "haskell" }
 -- Always installed on startup, useful for parsers without a strict filetype
 lvim.builtin.treesitter.ensure_installed = { "comment", "markdown_inline", "regex", "bash" }
 
--- Duplicate context_commentstring
+-- Deprecated context_commentstring
 lvim.builtin.treesitter.context_commentstring = nil
