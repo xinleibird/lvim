08cbbb05a08a1e74a6a8be3253f07eb573975b34
辛磊 <xinleibird@gmail.com>
2024-01-26 03:18:37 +0800

    Bump plugins

--- lua/config/lvim/treesitter.lua
+++ lua/config/lvim/treesitter.lua
@@ -3,7 +3,8 @@ lvim.builtin.treesitter.auto_install = true
 lvim.builtin.treesitter.ignore_install = { "haskell" }
 
 -- Always installed on startup, useful for parsers without a strict filetype
-lvim.builtin.treesitter.ensure_installed = { "comment", "markdown_inline", "regex", "bash" }
+lvim.builtin.treesitter.ensure_installed =
+  { "comment", "markdown", "markdown_inline", "regex", "bash", "lua", "vim" }
 
 -- Deprecated context_commentstring
 lvim.builtin.treesitter.context_commentstring = nil
