7362bd78d6365fcb7234b2a9d9dad1652a33b270
辛磊 <xinleibird@gmail.com>
2024-02-04 04:54:46 +0800

    Change alpha settings

--- lua/users/configs/lvim/alpha.lua
+++ lua/users/configs/lvim/alpha.lua
@@ -1,6 +1,16 @@
 -- Enable dashboard
 -- lvim.builtin.alpha.active = false
 
+local function reset_logo_hls()
+  local green = vim.g.terminal_color_2
+  local blue = vim.g.terminal_color_4
+
+  vim.api.nvim_set_hl(0, "NeovimDashboardLogo1", { fg = blue })
+  vim.api.nvim_set_hl(0, "NeovimDashboardLogo2", { fg = green, bg = blue })
+  vim.api.nvim_set_hl(0, "NeovimDashboardLogo3", { fg = green })
+  vim.api.nvim_set_hl(0, "NeovimDashboardLogo4", { fg = green, bold = true })
+end
+
 local pacman =
   "󰮯············································󱙝󰊠󱙝"
 local skills = "            󰟬  󱏿          󰎙      󰜫  󰘐"
@@ -18,9 +28,52 @@ local version = " Neovim ver "
   .. " LunarVim"
   .. " "
 
-lvim.builtin.alpha.dashboard.section.header.val = require("users.libs.banner").generate()
+-- lvim.builtin.alpha.dashboard.section.header.val = require("users.libs.banner").generate()
+lvim.builtin.alpha.dashboard.section.header.val = {
+  [[                 ]],
+  [[                 ]],
+  [[                 ]],
+  [[     █  █     ]],
+  [[     ██ ██     ]],
+  [[     █████     ]],
+  [[     ██ ███     ]],
+  [[     █  █     ]],
+  [[                 ]],
+  [[                 ]],
+  [[  N E O  V I M   ]],
+  [[                 ]],
+  [[                 ]],
+  [[                 ]],
+  [[                 ]],
+}
+
+lvim.builtin.alpha.dashboard.section.header.opts.hl = {
+  { { "", 0, 0 } },
+  { { "", 0, 0 } },
+  { { "", 0, 0 } },
+  { { "NeovimDashboardLogo1", 6, 8 }, { "NeovimDashboardLogo3", 9, 22 } },
+  {
+    { "NeovimDashboardLogo1", 6, 8 },
+    { "NeovimDashboardLogo2", 9, 11 },
+    { "NeovimDashboardLogo3", 12, 24 },
+  },
+  { { "NeovimDashboardLogo1", 6, 11 }, { "NeovimDashboardLogo3", 12, 26 } },
+  { { "NeovimDashboardLogo1", 6, 11 }, { "NeovimDashboardLogo3", 12, 24 } },
+  { { "NeovimDashboardLogo1", 6, 11 }, { "NeovimDashboardLogo3", 12, 22 } },
+  { { "", 0, 0 } },
+  { { "", 0, 0 } },
+  { { "NeovimDashboardLogo3", 0, 8 }, { "NeovimDashboardLogo4", 9, 17 } },
+}
+
 lvim.builtin.alpha.dashboard.section.footer.val = require("lvim.interface.text").align_center(
   { width = 0 },
   { "", "", pacman, skills, version },
   0
 )
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+  group = vim.api.nvim_create_augroup("neovim_alpha", {}),
+  callback = function()
+    reset_logo_hls()
+  end,
+})
