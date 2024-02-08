8970e75d779b770cb0f2eb33b27e5ff8b4c9962a
辛磊 <xinleibird@gmail.com>
2024-02-08 16:08:06 +0800

    Use hop.nvim only, remove eyeliner.nvim

--- lua/packages/hop.lua
+++ lua/packages/hop.lua
@@ -3,6 +3,8 @@ local M = {
   event = "BufRead",
   config = function()
     local hop = require "hop"
+    local directions = require("hop.hint").HintDirection
+
     hop.setup()
 
     vim.api.nvim_create_autocmd({ "FileType" }, {
@@ -11,7 +13,23 @@ local M = {
         if vim.bo.ft ~= "alpha" then
           vim.keymap.set("", "s", function()
             hop.hint_words()
-          end, { remap = true, buffer = true })
+          end, { remap = true, buffer = true, silent = true })
+
+          vim.keymap.set("", "f", function()
+            hop.hint_words { direction = directions.AFTER_CURSOR, current_line_only = true }
+          end, { remap = true, buffer = true, silent = true })
+
+          vim.keymap.set("", "F", function()
+            hop.hint_words { direction = directions.BEFORE_CURSOR, current_line_only = true }
+          end, { remap = true, buffer = true, silent = true })
+
+          vim.keymap.set("", "t", function()
+            hop.hint_words { direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 }
+          end, { remap = true, buffer = true, silent = true })
+
+          vim.keymap.set("", "T", function()
+            hop.hint_words { direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 }
+          end, { remap = true, buffer = true, silent = true })
         end
       end,
     })
