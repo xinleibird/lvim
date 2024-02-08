968c80e75bb7cd2b53848940450ca16804a5e0b3
辛磊 <xinleibird@gmail.com>
2024-02-09 07:49:30 +0800

    Update bufferline settings

--- lua/custom/lvim/bufferline.lua
+++ lua/custom/lvim/bufferline.lua
@@ -30,3 +30,35 @@ lvim.builtin.bufferline.options.custom_areas = {
     return result
   end,
 }
+
+lvim.builtin.bufferline.options.offsets = {
+  {
+    filetype = "undotree",
+    text = "Undotree",
+    highlight = "PanelHeading",
+    padding = 1,
+  },
+  {
+    filetype = "NvimTree",
+    text = "󱏒 Explorer",
+    highlight = "PanelHeading",
+    padding = 1,
+  },
+  {
+    filetype = "DiffviewFiles",
+    text = "Diff View",
+    highlight = "PanelHeading",
+    padding = 1,
+  },
+  {
+    filetype = "flutterToolsOutline",
+    text = "Flutter Outline",
+    highlight = "PanelHeading",
+  },
+  {
+    filetype = "lazy",
+    text = "Lazy",
+    highlight = "PanelHeading",
+    padding = 1,
+  },
+}
